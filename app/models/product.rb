43da59a877a21b85442e5d46b6f5d4a3c5d4ca0d
Juan Felipe <juanfrg2001@gmail.com>
2024-04-25 17:33:18 -0500

    Adds model validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
   belongs_to :store_section
+
+  #validations
+  validates :name, presence: true, length: { maximum: 128 }, format: { without: /[%$&@+|]/ }
+  validates :purchase_date, presence: true
+  validates :store_name, length: { maximum: 64 }
+  validates :store_section_id, presence: true, numericality: true
+  validates :purchased, inclusion: { in: [true, false] }
+  validates :quantity, presence: true, numericality: { only_integer: true }
 end
