ec60eb6965c5f86c58d532acfda8f04d203f9893
Juan Felipe <juanfrg2001@gmail.com>
2024-04-29 15:46:11 -0500

    Adds

--- build.sh
+++ build.sh
@@ -5,26 +5,7 @@ echo "Iniciando script de construcción..."
 
 if ! command -v ruby &> /dev/null; then
   echo "Ruby no está instalado. Instalando..."
-  if [ ! -d "$HOME/.rbenv" ]; then
-  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-  fi
-
-  # Agrega rbenv al PATH de tu shell
-  export PATH="$HOME/.rbenv/bin:$PATH"
-  eval "$(rbenv init -)"
-
-  # Clona el plugin ruby-build si no está instalado
-  if [ ! -d "$HOME/.rbenv/plugins/ruby-build" ]; then
-      git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-  fi
-
-  # Instala Ruby 3.2.3 si no está instalado
-  if ! rbenv versions | grep -q "3.2.3"; then
-      rbenv install 3.2.3
-  fi
-
-  # Configura la versión de Ruby como local en el directorio actual
-  rbenv local 3.2.3
+  ./install_ruby.sh
 fi
 
 # Verifica si Rails está instalado
