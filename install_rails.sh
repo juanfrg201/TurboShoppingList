b5c9a573ad7b008def921fd8fe0dd92ec0075785
Juan Felipe <juanfrg2001@gmail.com>
2024-05-01 19:20:30 -0500

    Adds modifications in rails 7.1.3 instalation

--- install_rails.sh
+++ install_rails.sh
@@ -5,16 +5,18 @@ echo "Instalando Ruby on Rails y sus dependencias"
 sudo apt-get update
 sudo apt-get install -y curl gnupg2 dirmngr
 
-# Instalación de RVM para manejar versiones de Ruby
-gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
-
 # Instalación de Node.js y Yarn para Rails
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update
 sudo apt-get install -y nodejs yarn
+
+echo "gem: --no-document" > ~/.gemrc
+
+gem install bundler
+
+gem install rails -v 7.1.3
 
 # Instalación de Rails
 gem install rails "7.1.3"
